2c8ef150a7b9a46e339c5b69108a0dc4a0ff93b4
Muhannad Issa <issapydev@gmail.com>
2020-06-24 19:38:55 -0400

    fix the index range

--- get-quote.py
+++ get-quote.py
@@ -3,11 +3,11 @@ import random
 def baby():
   
   with open("quotes.txt", 'a') as f:
-    f.write("\nWork smarter not harder")
+    #f.write("\nWork smarter not harder")
   #quotes = f.readlines()
-  f = open("quotes.txt")
+    f = open("quotes.txt")
   quotes = f.read().splitlines()
-  last = 15
+  last = 14
   rnd = random.randint(0, last)
 
   print(quotes[rnd])
